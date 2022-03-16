aeec6c36fbd1aa555104b69eba9c355f7ec0fbfa
Ariel Rokem <arokem@gmail.com>
2022-03-16 14:43:40 -0700

    Set up versioning with setuptools_scm.

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 from setuptools import setup
 import os.path as op
 
-opts = dict(package_data={'ndslib': [op.join('templates', '*')]})
+def local_version(version):
+    """
+    Patch in a version that can be uploaded to test PyPI
+    """
+    scm_version = get_version()
+    if "dev" in scm_version:
+        gh_in_int = []
+        for char in version.node:
+            if char.isdigit():
+                gh_in_int.append(str(char))
+            else:
+                gh_in_int.append(str(string.ascii_letters.find(char)))
+        return "".join(gh_in_int)
+    else:
+        return ""
+
+
+opts = dict(
+    use_scm_version={"root": ".", "relative_to": __file__,
+                     "write_to": op.join("ndslib", "version.py"),
+                     "local_scheme": local_version},
+                     package_data={'ndslib': [op.join('templates', '*')]}
+                     )
+
 
 if __name__ == '__main__':
     setup(**opts)
